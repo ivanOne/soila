f999cd037e9718d07dadce34290fd3dbdb26214a
IvanOne <turnthepage41@gmail.com>
2017-07-01 12:22:16 +0300

    pager views

--- blog/views.py
+++ blog/views.py
@@ -4,19 +4,31 @@ from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 
 
 # Create your views here.
-
+#Простой, но одновремено некрасивый способ описывать view с пагинацией
 def post_list_by_category(request, slug_category):
     categories = Category.objects.all()
-    posts = Post.objects.all().order_by('-published_date')
-    if slug_category:
-        category = get_object_or_404(Category, slug=slug_category)
-        posts = posts.filter(category=category)
-    args = {}
+    category = get_object_or_404(Category, slug=slug_category)
+    posts = Post.objects.filter(category=category).order_by('-published_date')
+    recent_posts = posts[0:3]
+
+    #Готовим пагинатор, закидываем в него посты, и определяем по сколько записей выводить
+    paginator = Paginator(posts, 1)
+    page = request.GET.get('page')
+    try:
+        #Нормальная работа пагинатора закидываем страницу получаем результат
+        posts = paginator.page(page)
+    except PageNotAnInteger:
+        #в гет попапа какая то хрень, показываем первую страницу
+        posts = paginator.page(1)
+    except EmptyPage:
+        #Например страниц у нас 10 а кто то решил запросить 11ю шлем ему последнюю страницу
+        posts = paginator.page(paginator.num_pages)
+
+    args = dict()
     args['posts'] = posts
     args['category'] = category
     args['categories'] = categories
-    args['recent_posts'] = posts[0:3]
-
+    args['recent_posts'] = recent_posts
     return render_to_response('post_list_by_category.html', args)
 
 
